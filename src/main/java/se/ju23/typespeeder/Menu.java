5827279d6a3f348ba4db4bd83b29d8e7a25dc53d
Kayuta96 <serafu5546@hotmail.com>
2024-02-11 20:11:50 +0100

    Added a menu class and the basics.

--- src/main/java/se/ju23/typespeeder/Menu.java
+++ src/main/java/se/ju23/typespeeder/Menu.java
@@ -1,4 +1,25 @@
 package se.ju23.typespeeder;
 
 public class Menu {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] menyChoices = {
+                "1.",
+                "2.",
+                "3.",
+                "4.",
+                "5."
+        };
+        while (true) {
+            System.out.println("------------");
+            System.out.println("Välkommen till huvudmenyn.");
+            for (String menyChoice : menyChoices)
+                System.out.println(menyChoice);
+
+            int userInput = scanner.nextInt();
+        }
+    }
 }
