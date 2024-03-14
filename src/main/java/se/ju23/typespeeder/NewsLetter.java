6a8e85587523d90e2a04235fb549b337fd45c341
Kayuta96 <serafu5546@hotmail.com>
2024-03-15 00:34:59 +0100

    Adderat patch.java och fixat newsletter

--- src/main/java/se/ju23/typespeeder/NewsLetter.java
+++ src/main/java/se/ju23/typespeeder/NewsLetter.java
@@ -1,15 +1,15 @@
 package se.ju23.typespeeder;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class NewsLetter {
-
     private String content;
     private LocalDateTime publishDateTime;
 
-    public NewsLetter() {
-        this.content = ""; // Tomt innehåll som måste uppdateras senare
-        this.publishDateTime = LocalDateTime.now(); // Sätt publiceringstiden till nuvarande tid
+    public NewsLetter(String content, LocalDateTime publishDateTime) {
+        this.content = content;
+        this.publishDateTime = publishDateTime;
     }
 
     public String getContent() {
@@ -18,5 +18,12 @@ public class NewsLetter {
 
     public LocalDateTime getPublishDateTime() {
         return publishDateTime;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = publishDateTime.format(formatter);
+        return "Content: " + content + "\nPublished: " + formattedDateTime;
     }
 }
